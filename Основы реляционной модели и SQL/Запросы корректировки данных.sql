b7b750652e6dc3b79c9d95359ccc8e4e2a7aec75
Ivan Kazakov <ivankaz9@yandex.ru>
2022-06-16 20:01:30 +0400

    Решил 3 задачи по корректировке данных

--- Основы реляционной модели и SQL/Запросы корректировки данных.sql
+++ Основы реляционной модели и SQL/Запросы корректировки данных.sql
@@ -22,3 +22,32 @@ SELECT title, author, price, amount
 FROM supply
 WHERE author NOT IN ('Булгаков М.А.', 'Достоевский Ф.М.');
 -- SELECT * FROM book;
+
+-- добавляю в таблицу book книги из таблицы supply только тех авторов,
+-- которых нет в таблице book
+INSERT INTO book(title, author, price, amount)
+SELECT title, author, price, amount
+FROM supply
+WHERE author NOT IN (
+    -- вложенный запрос, которым выбираю авторов таблицы book
+    SELECT DISTINCT author
+    FROM book
+    );
+-- SELECT * FROM book;
+
+-- снижаю цену на 10% для тех книг,
+-- количество которых от 5 до 10 включительно
+UPDATE book
+SET price = 0.9 * price
+WHERE amount BETWEEN 5 AND 10;
+-- SELECT * FROM book;
+
+-- обновляю таблицу book
+UPDATE book
+-- если заказ покупателя больше количества экземпляров в наличии,
+-- то устанавливаю заказ равным количеству всех экземпляров книги
+SET buy = IF(buy > amount, amount, buy),
+-- если на книгу нет заказов,
+-- то уменьшаю цену на 10%
+    price = IF(buy = 0, price * 0.9, price);
+-- SELECT * FROM book;
