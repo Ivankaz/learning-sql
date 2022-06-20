603923ed0f2fcce81f52337a26212ab0df05483f
Ivan Kazakov <ivankaz9@yandex.ru>
2022-06-20 23:53:37 +0400

    Решил 5 задач по запросам на выборку

--- Основы реляционной модели и SQL/Таблица "Командировки", запросы на выборку.sql
+++ Основы реляционной модели и SQL/Таблица "Командировки", запросы на выборку.sql
@@ -36,3 +36,49 @@ FROM trip
 WHERE city NOT IN ('Москва', 'Санкт-Петербург')
 -- сортирую по убыванию длительности и города (в обратном алфавитном порядке)
 ORDER BY Длительность DESC, city DESC;
+
+-- выбираю командировки сотрудника(-ов),
+-- которые были наименьшей длительности
+SELECT name, city, date_first, date_last
+FROM trip
+WHERE (DATEDIFF(date_last, date_first) + 1) = (
+    -- вложенный запрос, которым я получаю минимальную длительность командировки
+    -- длительность командировки подсчитываю с помощью функции DATEDIFF
+    SELECT MIN(DATEDIFF(date_last, date_first) + 1) FROM trip
+    );
+
+-- выбираю записи о командировках,
+-- которые начались и закончились в одном месяце (при этом год может быть разным)
+SELECT name, city, date_first, date_last
+FROM trip
+-- с помощью функции MONTH получаю номер месяца
+WHERE MONTH(date_first) = MONTH(date_last)
+-- сортирую записи сначала по городу, потом по фамилии сотрудника
+ORDER BY city ASC, name ASC;
+
+-- подсчитываю количество командировок для каждого месяца
+-- функция MONTHNAME возвращает название месяца на английском языке
+SELECT MONTHNAME(date_first) AS Месяц, COUNT(date_first) AS Количество
+FROM trip
+GROUP BY MONTHNAME(date_first)
+-- сортирую по убыванию количества командировок,
+-- и затем по названию месяца в алфавитном порядке
+ORDER BY Количество DESC, Месяц ASC;
+
+-- подсчитываю сумму суточных для командировок в феврале и марте 2020 года
+SELECT name, city, date_first,
+       (DATEDIFF(date_last, date_first) + 1) * per_diem AS Сумма
+FROM trip
+WHERE MONTH(date_first) IN (2, 3) AND YEAR(date_first) = 2020
+-- сортирую по фамилии сотрудника в алфавитном порялке и по убыванию суммы суточных
+ORDER BY name ASC, Сумма DESC;
+
+-- вывожу суммы суточных сотрудников
+SELECT name,
+       SUM((DATEDIFF(date_last, date_first) + 1) * per_diem) AS Сумма
+FROM trip
+GROUP BY name
+-- оставляю только тех сотрудников, которые были в командировках более 3 раз
+HAVING COUNT(name) > 3
+-- сортирую по убыванию суммы суточных
+ORDER BY Сумма DESC;
