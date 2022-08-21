a699bfab39602812dbbb3345b06c655de6c36217
Ivan Kazakov <ivankaz9@yandex.ru>
2022-08-21 12:03:20 +0400

    Добавил заказ для клиента

--- Запросы SQL к связанным таблицам/Запросы корректировки к базе данных "Интернет-магазин книг".sql
+++ Запросы SQL к связанным таблицам/Запросы корректировки к базе данных "Интернет-магазин книг".sql
@@ -5,3 +5,11 @@ FROM city
 WHERE name_city = 'Москва';
 
 -- SELECT * FROM client;
+
+-- добавляю заказ для клиента Попов Илья
+INSERT INTO buy(buy_description, client_id)
+SELECT 'Связаться со мной по вопросу доставки', с.client_id
+FROM client с
+WHERE name_client = 'Попов Илья';
+
+-- SELECT * FROM buy;
